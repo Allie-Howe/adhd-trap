2d0bcc101567de2c3fb95f7c99d71848bac84fb7
Allie Howe <howeisallie@gmail.com>
2024-09-14 09:23:58 +0100

    initial rotating circle

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import P5 from 'p5'
 
+const DIAMETER = 100
+const SPEED = 0.002
+
 const sketch = (p5: P5) => {
   p5.windowResized = () => {
     p5.resizeCanvas(window.innerWidth, window.innerHeight)
@@ -9,14 +12,26 @@ const sketch = (p5: P5) => {
     p5.createCanvas(window.innerWidth, window.innerHeight)
     p5.frameRate(60)
 
-    p5.rectMode(p5.CENTER)
+    p5.stroke(255)
+    p5.noFill()
+    p5.strokeWeight(2)
   }
 
   p5.draw = () => {
     p5.background(0);
     p5.translate(p5.width / 2, p5.height / 2)
 
-    p5.rect(0, 0, 100, 100)
+    p5.circle(0, 0, DIAMETER)
+
+
+    const xTrans = p5.sin(p5.millis() * SPEED) * DIAMETER/2
+    const yTrans = -p5.cos(p5.millis() * SPEED) * DIAMETER/2
+    p5.translate(xTrans, yTrans)
+
+    p5.push()
+    p5.fill(255)
+    p5.circle(0, 0, 20)
+    p5.pop()
   }
 }
 
