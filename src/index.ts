a4e1658c8d1f4a7f5b37d09f3b5e33994cf22254
Allie Howe <howeisallie@gmail.com>
2024-09-16 08:47:10 +0100

    adding dashed lines

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { range } from 'lodash'
 import P5 from 'p5'
 import { colours } from './colours'
 import { drawCircle } from './drawCircle'
+import { INITIAL_RADIUS } from './vars'
 
 const sketch = (p5: P5) => {
   p5.windowResized = () => {
@@ -20,6 +21,13 @@ const sketch = (p5: P5) => {
   p5.draw = () => {
     p5.background(0);
     p5.translate(p5.width / 2, p5.height / 2)
+
+    p5.push()
+    p5.drawingContext.setLineDash([5]);
+    p5.line(0, INITIAL_RADIUS, 0, INITIAL_RADIUS * colours.length);
+    p5.line(0, -INITIAL_RADIUS, 0, -INITIAL_RADIUS * colours.length);
+    p5.pop()
+
 
     range(colours.length).forEach(i => {
       drawCircle(p5, i)
